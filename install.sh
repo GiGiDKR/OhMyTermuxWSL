6573523e2f001df512a67af8b7a0d32926fcf007
OhMyApps <74984020+GiGiDKR@users.noreply.github.com>
2024-10-09 01:26:05 +0200

    dev

--- install.sh
+++ install.sh
@@ -260,6 +260,13 @@ fi
 
 execute_command "sudo usermod -aG docker $USER" "Ajout de l'utilisateur $USER au groupe Docker" 10
 
+# Redémarrage du service Docker
+if is_wsl; then
+    execute_command "sudo service docker restart" "Redémarrage du service Docker" 30
+else
+    execute_command "sudo systemctl restart docker" "Redémarrage du service Docker" 30
+fi
+
 #execute_command "sleep 10" "Pause de 10 secondes" 10
 
 # Rechargement des groupes de l'utilisateur
